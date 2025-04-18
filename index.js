2a89c7d1348bd1fb9f2c9b0e55e1994a5e58d016
Heena Kumawat <heenakumawat23k@gmail.com>
2025-04-18 08:13:22 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// to create an server
+const express = require('express');
+// create an instance of express
+const app = express();
+const path = require('path');
+app.get('/',(req,res)=>{
+    res.send('<h1> Hello Word!</h1>')
+})
+app.get('/contact',(req,res)=>{
+    res.send('<h1> contact</h1>')
+})
+app.get('/files',(req,res)=>{
+    res.sendFile(path.join(__dirname,'index.html'))
+})
+app.get('/jsonexample',(req,res)=>{
+    res.json([{name:'John',age:30,city:'New York'},
+{name:'Jane',age:25,city:'Los Angeles'}, {name:'Mike',age:35,city:'Chicago'}])
+    
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+})
